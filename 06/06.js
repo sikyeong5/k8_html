d9ccdf912e3238b5c0dbcf46525a44a2260e02a3
sikyeong5 <ojm3723@naver.com>
2024-09-03 16:50:30 +0900

    HTML

--- 06/06.js
+++ 06/06.js
@@ -1,12 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const bt = document.querySelector('button');
-  const img = document.querySelector('#msg > img');
 
-  bt1.addEventListener('click', () => {
-    let n = Math.floor(Math.random() * 6) + 1;
+  const imgs = document.querySelectorAll(`.dice > p > img`);
 
-    img.setAttribute('src', `../img/${n}.png`);
-    img.setAttribute('alt', `${n}.png`);
-  });
+  const bts = document.querySelectorAll('.bt4');
+
+  const msg = document.querySelector('#msg');
+
+  console.log(bts)
+
+  //버튼 클릭
+  for (let bt of bts) {
+    console.log(bt)
+    bt.addEventListener(`click`, () => {
+      //컴퓨터 랜덤수
+      let comN = Math.floor(Math.random() * 6) + 1;
+      imgs[0].setAttribute('src',`../img/${comN}.png`);
+      imgs[0].setAttribute('alt',`${comN}.png`);
+
+      //사용자 선택수
+      console.log(bt.textContent.charAt(0))
+      let userN = parseInt(bt.textContent.charAt(0));
+      imgs[1].setAttribute('src',`../img/${userN}.png`);
+      imgs[1].setAttribute('alt',`${userN}.png`);
+
+      
+      //결과표시
+      if(comN === userN) {
+        msg.textContent ='맞음';
+      } else {
+        msg.textContent ='틀림';
+      }
+    });
+  }
+
 });
-
