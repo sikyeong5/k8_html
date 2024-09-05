c75845b1c3f8c8e2b88efa8672090424562fbff0
sikyeong5 <ojm3723@naver.com>
2024-09-05 15:28:28 +0900

    HTML

--- 08/08.js
+++ 08/08.js
@@ -1,21 +1,42 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-const img = document.querySelector('alt> img');
-const txt = document.querySelector('#txt1');
-const bt1 = document.querySelector('#bt1');
+document.addEventListener('DOMContentLoaded', () => {
+  const img = document.querySelector('.pic> img');
+  const txt1 = document.querySelector('#txt1');
+  const bt1 = document.querySelector('#bt1');
 
-//랜덤수
-let n;
+  //랜덤수
+  let n;
+  //flag변수
+  let flag = false;
 
-bt1.addEventListener('click',(e)=>{
-  e.preventDefault();
-  n = Math.floor(Math.random()*100)+ 1;
-  if (n === parseFloat(txt1.value)) {
-    img.setAttribute('scr', '../img/good.png');
-  } else if {
-    img.setAttribute('scr', '../img/up.png');
-   } else {
-    img.setAttribute('scr', '../img/down.png');
-  }
-});
+  bt1.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    //if (flag == false) {
+    if (!flag) {
+      n = Math.floor(Math.random() * 100) + 1;
+      console.log('n=', n);
+      flag = true;
+
+      if (txt1.style.display == 'none') {
+        txt1.style.display = 'inline';
+        img.setAttribute('src', '../img/what.png');
+        txt1.value = '';
+        txt1.focus();
+        bt1.innerHTML = '확인';
+        return;
+      }
+    }
+
+    if (n === parseFloat(txt1.value)) {
+      img.setAttribute('src', '../img/good.png');
+      txt1.style.display = 'none';
+      bt1.innerHTML = '번호를 다시 생성하세요'
+      flag = false;
+    } else if (n > parseFloat(txt1.value)){
+      img.setAttribute('src', '../img/up.png');
+    } else {
+      img.setAttribute('src', '../img/down.png');
+    }
+  });
 
 });
