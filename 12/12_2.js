56fb7c092bd3498ebb9874b9785b2fcf0f3fb859
sikyeong5 <ojm3723@naver.com>
2024-09-12 13:10:04 +0900

    HTML

--- 12/12_2.js
+++ 12/12_2.js
@@ -1,11 +1,54 @@
+//전역변수
+const testAPI = '82ca741a2844c5c180a208137bb92bd7';
+
+
+//상세정보 가져오기
+const getDetail = (movieCd) => {
+  const mvinfo = document.querySelector('#mvinfo');
+  let url = `https://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?`;
+  url = `${url}key=${testAPI}&movieCd=${movieCd}`;
+
+  // movieinfo.innerHTML = movieCd;
+ 
+
+  console.log(url);
+
+  fetch(url) //가져와서 던지기
+    .then(resp => resp.json())
+    .then(data => {
+     
+      let movieInfo = data.movieInfoResult.movieInfo ;
+      let genres = movieInfo.genres.map(item => item.genreNm).join(',');
+      let companys =movieInfo.companys.map(item =>`${item.companyNm} (${item.companypartNm})`).join(', ');
+      console.log(genres)
+      console.log(companys)
+
+
+      mvinfo.innerHTML = `
+      <div>${movieInfo.movieNm} (${movieInfo.openDt})</div>
+      <ul>
+      <li> 장르: ${genres}</li>
+      <li> 제작사: ${companys}</li>
+      </ul>
+      `;
+    })
+
+    .catch(err => console.log(err))
+
+  mvinfo.innerHTML = movieCd;
+
+
+};
+
+
 //OPEN api 데이터 가져오기
 const getData = (selDt, ul, gubun) => {
-  console.log(selDt);
+  // console.log(selDt);
 
-  const testAPI = '82ca741a2844c5c180a208137bb92bd7';
+
   let url = `http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?`;
   url = `${url}key=${testAPI}&targetDt=${selDt}`;
- 
+
   if (gubun != 'T') {
     url = `${url}&repNationCd=${gubun}`;
   }
@@ -21,20 +64,22 @@ const getData = (selDt, ul, gubun) => {
 
 
       let tm = dailyBoxOfficeList.map(item =>
-        `<li class='mvli'>
+        `<a href="#" onclick="getDetail(${item.movieCd});"> 
+        <li class='mvli'>
         <span class='rank'>${item.rank}</span>
         <span class='movieNm'>${item.movieNm}</span>
         <span class='openDt'>${item.openDt}</span>
         <span class='rankInten'>
 
 
-        ${item.rankInten > 0 ? 
-          '<span class= "spRed">▲</span>' : item.rankInten < 0?
-                 '<span class= "spBlue">▼</span>' : ' - '}
-        ${item.rankInten !=0 ? Math.abs(item.rankInten) : ''}
+        ${item.rankInten > 0 ?
+          '<span class= "spRed">▲</span>' : item.rankInten < 0 ?
+            '<span class= "spBlue">▼</span>' : ' - '}
+        ${item.rankInten != 0 ? Math.abs(item.rankInten) : ''}
         </span>
 
-      </li>`)
+      </li></a>
+      `)
 
 
       tm = tm.join('')
@@ -76,22 +121,22 @@ const getYesterday = () => {
 
 
 //radio 값 가져오기
- const getGubun = () => {
-// //radio 요소 가져오기
-// const r1 = document.querySelector('#r1');
-// const r2 = document.querySelector('#r2');
-// const r3 = document.querySelector('#r3');
+const getGubun = () => {
+  // //radio 요소 가져오기
+  // const r1 = document.querySelector('#r1');
+  // const r2 = document.querySelector('#r2');
+  // const r3 = document.querySelector('#r3');
 
-// console.log("r1=", r1.checked);
-// console.log("r2=", r2.checked);
-// console.log("r3=", r3.checked);
+  // console.log("r1=", r1.checked);
+  // console.log("r2=", r2.checked);
+  // console.log("r3=", r3.checked);
 
-// if (r1.checked) return r1.value;
-// else if(r2.checked) return r2.value;
-// else if(r3.checked) return r3.value;
+  // if (r1.checked) return r1.value;
+  // else if(r2.checked) return r2.value;
+  // else if(r3.checked) return r3.value;
 
   //radio 버튼의 클릭된 것만 가져오기 (위에 있는 코드를 이걸로 대체 가능)
-  const gubun=document.querySelector('input[name= mvGubun]:checked');
+  const gubun = document.querySelector('input[name= mvGubun]:checked');
   console.log('gubun=', gubun.value);
 
   return gubun.value;
@@ -114,7 +159,7 @@ document.addEventListener('DOMContentLoaded', () => {
   //const radios= document.getElementsByName('mvGubun');
 
 
-  const radios= document.querySelectorAll('input[name= mvGubun]');
+  const radios = document.querySelectorAll('input[name= mvGubun]');
 
 
 
@@ -133,31 +178,22 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log(getGubun());
 
 
-   //기본 첫 페이지 보이기
-   getData(dt.value.replaceAll('-',''), ul, getGubun());
+  //기본 첫 페이지 보이기
+  getData(dt.value.replaceAll('-', ''), ul, getGubun());
 
   //데이터 가져오기
   dt.addEventListener('change', () => {
     getData(dt.value.replaceAll('-', ''), ul, getGubun());
   });
 
- 
-for (let radio of radios) {
-  radio.addEventListener('click', ()=>{
-    if(radio.checked) 
-      getData(dt.value.replaceAll('-', ''), ul, radio.value);
-  });
-}
 
-  // const date = new Date(yesterday);
-  // const dateFormat = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
-  // console.log(dateFormat);
+  for (let radio of radios) {
+    radio.addEventListener('click', () => {
+      if (radio.checked)
+        getData(dt.value.replaceAll('-', ''), ul, radio.value);
+    })
+  }
 
 
 
-
-  
 });
-
-
-
